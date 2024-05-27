3c38a7ca363eb24a211cf36d5a29c1fe5698465f
Arin <arin.apichitchon@gmail.com>
2024-05-27 15:16:44 -0400

    first Vue.js

--- web7/app.js
+++ web7/app.js
@@ -1,13 +1,28 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+// using vanilla JS
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
+// buttonEl.addEventListener('click', addGoal);
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
-}
-
-buttonEl.addEventListener('click', addGoal);
+// using Vue
+Vue.createApp({
+  data () {
+    return {
+      goals: [],
+      enteredValue: ''
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = ''; // clear input
+    }
+  }
+}).mount('#app');
